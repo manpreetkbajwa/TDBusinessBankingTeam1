2a7d5625d06f9bd8997676444a8d18b4fd6df549
Monika Tummala <bmonika0201@gmail.com>
2020-07-23 22:42:49 -0400

    Added Xpath for PlanA

--- src/main/java/TDPages/LandingPage.java
+++ src/main/java/TDPages/LandingPage.java
@@ -1,5 +1,8 @@
 package TDPages;
 
+import org.openqa.selenium.By;
+
 public class LandingPage {
 
+	 private By PlanAViewDetails = By.xpath("//section[@id='catalogue1_product0']/div/div[2]/div/p/a");
 }
--- src/main/test/TDTest/TDPlanA.java
+++ src/main/test/TDTest/TDPlanA.java
@@ -1,10 +1,13 @@
 package TDTest;
 
+import org.openqa.selenium.By;
+
 public class TDPlanA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+ //private By PlanAViewDetails = By.xpath("//section[@id='catalogue1_product0']/div/div[2]/div/p/a");
 	}
 
 }
