b716d33163029e26fbe4bfb3bed3f5b3b5756aba
Monika Tummala <bmonika0201@gmail.com>
2020-07-24 01:22:48 -0400

    changes made in master

--- src/main/java/TDPages/LandingPage.java
+++ src/main/java/TDPages/LandingPage.java
@@ -5,32 +5,29 @@ import org.openqa.selenium.WebDriver;
 
 public class LandingPage extends BasePage {
 
-	private static final By PlanA = null;
-	private static final By PlanB = null;
-	private static final By PlanC = null;
-	private static final By BasicBusinessPlan = null;
-	private By PlanAViewDetails = By.xpath("//section[@id='catalogue1_product0']/div/div[2]/div/p/a");
+	
+	 private By PlanAViewDetails = By.xpath("//section[@id='catalogue1_product0']/div/div[2]/div/p/a");
 	 private By PlanBViewDetails = By.xpath("//section[@id='catalogue1_product1']/div/div[2]/div/p/a");
 	 private By PlanCViewDetails = By.xpath("//section[@id='catalogue1_product2']/div/div[2]/div/p/a");
 	 private By PlanBusinessBasicPlan = By.xpath("//section[@id='catalogue1_product3']/div/div[2]/div/p/a");
 	 
 	 public LandingPage(WebDriver driver) {
 			super(driver);
-		}
-
-		public void PlanAViewDetails() throws Throwable  {
-			getElementAndClick(PlanA);
-		}
-		
-		public void PlanBViewDetails() throws Throwable  {
-			getElementAndClick(PlanB);
-		}
-		public void PlanCViewDetails() throws Throwable  {
-			getElementAndClick(PlanC);
-		}
-		public void PlanBusinessBasicPlan() throws Throwable  {
-			getElementAndClick(BasicBusinessPlan);
-		}
+	 }
+//
+//		public void PlanAViewDetails() throws Throwable {
+//			getElementAndClick(PlanAViewDetails);
+//		}
+//		
+//		public void PlanBViewDetails() throws Throwable  {
+//			getElementAndClick(PlanBViewDetails);
+//		}
+//		public void PlanCViewDetails() throws Throwable  {
+//			getElementAndClick(PlanCViewDetails);
+//		}
+//		public void PlanBusinessBasicPlan() throws Throwable  {
+//			getElementAndClick(PlanBusinessBasicPlan);
+//		}
 		
 		}
 	 
